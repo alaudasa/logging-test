e11355a37575f968b58b1fa3554feeab006c3ea0
Mingqi Shao <shaomq@gmail.com>
2017-05-01 07:26:33 +0800

    change

--- logging.sh
+++ logging.sh
@@ -3,7 +3,7 @@
 mkdir -p /var/logging-test/
 while :
 do
-	echo `date +'%F %T'` "this is a logging message"
-	echo `date +'%F %T'` "this is a logging message" >> /var/logging-test/info.log
+	echo `date +'%F %T'` "this is a stdout logging message"
+	echo `date +'%F %T'` "this is a file logging message" >> /var/logging-test/info.log
 	sleep 3
 done
