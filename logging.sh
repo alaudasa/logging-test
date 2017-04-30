5fa079a03e9c247ffa40187f5c1c02c1b7466083
Mingqi Shao <shaomq@gmail.com>
2017-04-30 21:50:42 +0800

    add file log

--- logging.sh
+++ logging.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+mkdir -p /var/logging-test/
 while :
 do
 	echo `date +'%F %T'` "this is a logging message"
-	sleep 1
+	echo `date +'%F %T'` "this is a logging message" >> /var/logging-test/info.log
+	sleep 3
 done
